a11a26ff7f48da74cfece7c8aa31356ffe5eef95
Alina Ahmed <alina@AFs-MacBook-Pro.local>
2025-01-06 05:11:31 +0000

    Task2: Implement Search

--- todo-list-frontend/src/app/app.component.ts
+++ todo-list-frontend/src/app/app.component.ts
@@ -1,6 +1,10 @@
 import {Component} from '@angular/core';
 import {Todo, TodoService} from "./todo.service";
 import {Observable} from "rxjs";
+import { BehaviorSubject } from "rxjs";
+import { combineLatest } from "rxjs";
+import { map } from "rxjs/operators";
+
 
 @Component({
   selector: 'app-root',
@@ -12,9 +16,9 @@ import {Observable} from "rxjs";
     </div>
     <div class="list">
       <label for="search">Search...</label>
-      <input id="search" type="text">
+        <input id="search" type="text" (input)="onSearch($any($event.target).value)">
       <app-progress-bar></app-progress-bar>
-      <app-todo-item *ngFor="let todo of todos$ | async" [item]="todo"></app-todo-item>
+      <app-todo-item *ngFor="let todo of filteredTodos$ | async" [item]="todo"></app-todo-item>
     </div>
   `,
   styleUrls: ['app.component.scss']
@@ -23,7 +27,22 @@ export class AppComponent {
 
   readonly todos$: Observable<Todo[]>;
 
+  private searchTermSubject = new BehaviorSubject<string>('');
+  readonly filteredTodos$: Observable<Todo[]>;
+
   constructor(todoService: TodoService) {
     this.todos$ = todoService.getAll();
+
+    this.filteredTodos$ = combineLatest([this.todos$, this.searchTermSubject]).pipe(
+      map(([todos, searchTerm]) =>
+        todos.filter(todo =>
+          todo.task.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+      )
+    );
+  }
+
+  onSearch(searchTerm: string): void {
+    this.searchTermSubject.next(searchTerm);
   }
 }
